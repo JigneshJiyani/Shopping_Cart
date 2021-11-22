ba5db037df91dadb3ed0ad315306074e063440b9
JigneshJiyani <jigneshj.dotinfotech@gmail.com>
2021-11-22 12:59:05 +0530

    Change In ShoppingCart

--- src/ShoppingCartApp/ShoppingCart.js
+++ src/ShoppingCartApp/ShoppingCart.js
@@ -2,7 +2,8 @@ import { Container, Row, Col, Form, Offcanvas } from 'react-bootstrap';
 import Category from './Category';
 import List from './List';
 import Cart from './Cart';
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import Button from '@restart/ui/esm/Button';
 
 function MainSlide() {
 
@@ -113,6 +114,35 @@ function MainSlide() {
     );
 }
 
+//----------------------------------Greetings Menu------------------------------------------//
+
+function Greeting() {
+
+    const [firstName, setFirstName] = useState(() =>
+      window.localStorage.getItem('hooksFirstName') || ''
+    );
+    const [lastName, setLastName] = useState(() =>
+      window.localStorage.getItem('hooksLastName') || ''
+    );
+    const handleFirstNameChange = (e) => setFirstName(e.target.value);
+    const handleLastNameChange = (e) => setLastName(e.target.value);
+  
+    useEffect(() => {
+      window.localStorage.setItem('hooksFirstName', firstName);
+      window.localStorage.setItem('hooksLastName', lastName);
+    });
+  
+    return (
+      <div>
+        <input value={firstName} onChange={handleFirstNameChange} /><br />
+        <input value={lastName} onChange={handleLastNameChange} />
+        <p>
+          Hello, <span>{firstName} {lastName}</span>
+        </p>
+      </div>
+    );
+  }
+
 //-----------------------------------------Shopping Cart----------------------------------------//
 
 function Shoppingcart() {
@@ -207,7 +237,13 @@ function Shoppingcart() {
                                 handleSelect={handleSelect}
                                 cart={data}
                             />
-                            <MainSlide />
+                            <MainSlide/>
+                            <br/>
+                            <br/>
+                            <Button><b>Greetings Bar</b></Button>
+                            <br/>
+                            <br/>
+                            <Greeting/>
                         </Col>
                         <Col md={6}><List
                             key={countdata}
--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Shoppingcart from './ShoppingCartApp/ShoppingCart';
+import Greeting from './Greetings/grettings';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <Shoppingcart/>
+    <Shoppingcart />
   </React.StrictMode>,
   document.getElementById('root')
 );
